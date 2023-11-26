693e325ef91e0d7fbe8660df81b1f8a0109d7f11
iron-condor <38388619+iron-condor@users.noreply.github.com>
2023-11-25 19:54:53 -0500

    Cleaned up some code

--- app/src/main/java/com/condor/breadtrackermobile/FCMService.kt
+++ app/src/main/java/com/condor/breadtrackermobile/FCMService.kt
@@ -1,23 +1,22 @@
 package com.condor.breadtrackermobile
 
-import android.app.Notification
-import android.app.Notification.Action
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
-import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
-import android.os.Bundle
-import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
+
+const val STARTER_NOTIF_ID = 1;
+private const val STARTER_NAME_KEY = "starter_name";
+private const val STARTER_UUID_KEY = "starter_uuid";
 class FCMService : FirebaseMessagingService()  {
 
     override fun onMessageReceived(message: RemoteMessage) {
@@ -32,8 +31,8 @@ class FCMService : FirebaseMessagingService()  {
     }
 
     private fun processFeedStarterReminder(data: Map<String, String>) {
-        var starterUUID = data["starter_uuid"];
-        var starterName = data["starter_name"];
+        var starterUUID = data[STARTER_UUID_KEY];
+        var starterName = data[STARTER_NAME_KEY];
 
         val intent = Intent(this, MainActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
@@ -49,7 +48,7 @@ class FCMService : FirebaseMessagingService()  {
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val feedStarterAction = NotificationCompat.Action.Builder(
             R.mipmap.ic_launcher,
-            "Feed",
+            getString(R.string.feed_starters_feed_button_label),
             PendingIntent.getService(applicationContext, 0,
                 Intent(
                     ACTION_FEED, Uri.EMPTY, this, FeedStarterService::class.java)
@@ -59,14 +58,14 @@ class FCMService : FirebaseMessagingService()  {
             .build();
         val dismissAction = NotificationCompat.Action.Builder(
             R.mipmap.ic_launcher,
-            "Dismiss",
+            getString(R.string.feed_starters_dismiss_button_label),
             PendingIntent.getService(applicationContext, 0, Intent(ACTION_DISMISS, Uri.EMPTY, this, FeedStarterService::class.java),
                 PendingIntent.FLAG_IMMUTABLE))
             .build();
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
             .setSmallIcon(R.mipmap.ic_launcher)
-            .setContentTitle("Feed sourdough starters")
-            .setContentText("It's time to feed $starterName")
+            .setContentTitle(getString(R.string.feed_starters_notif_title_label))
+            .setContentText(getString(R.string.feed_starters_notif_body, starterName))
             .setAutoCancel(true)
             .setSound(defaultSoundUri)
             .setContentIntent(pendingIntent)
@@ -79,15 +78,13 @@ class FCMService : FirebaseMessagingService()  {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val channel = NotificationChannel(
                 channelId,
-                "Channel human readable title",
-                NotificationManager.IMPORTANCE_DEFAULT,
+                "BreadTracker notification channel",
+                NotificationManager.IMPORTANCE_LOW,
             )
             notificationManager.createNotificationChannel(channel)
         }
 
-        // TODO: Replace 0 with the notification ID
-        val notificationId = 0
-        notificationManager.notify(notificationId, notificationBuilder.build())
+        notificationManager.notify(STARTER_NOTIF_ID, notificationBuilder.build())
     }
 
 
--- app/src/main/java/com/condor/breadtrackermobile/FeedStarterService.kt
+++ app/src/main/java/com/condor/breadtrackermobile/FeedStarterService.kt
@@ -11,24 +11,26 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.net.URL
 
-// TODO: Rename actions, choose action names that describe tasks that this
 // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-public const val ACTION_FEED = "com.condor.breadtrackermobile.action.FEED"
-public const val ACTION_DISMISS = "com.condor.breadtrackermobile.action.DISMISS"
+const val ACTION_FEED = "com.condor.breadtrackermobile.action.FEED"
+const val ACTION_DISMISS = "com.condor.breadtrackermobile.action.DISMISS"
 
-// TODO: Rename parameters
-public const val STARTER_UUID = "com.condor.breadtrackermobile.extra.STARTER_UUID"
-public const val STARTER_NAME = "com.condor.breadtrackermobile.extra.STARTER_NAME"
+const val STARTER_UUID = "com.condor.breadtrackermobile.extra.STARTER_UUID"
+const val STARTER_NAME = "com.condor.breadtrackermobile.extra.STARTER_NAME"
 
 /**
  * An [IntentService] subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
-
- * TODO: Customize class - update intent actions, extra parameters and static
- * helper methods.
-
  */
 class FeedStarterService : IntentService("FeedStarterService") {
+
+    companion object Constants {
+        private const val SERVICE_HOST = "192.168.1.68"
+        private const val SERVICE_PORT = "8080"
+        private const val SERVICE_PROTOCOL = "http"
+
+        const val SERVICE_URL = "$SERVICE_PROTOCOL://$SERVICE_HOST:$SERVICE_PORT"
+    }
     override fun onHandleIntent(intent: Intent?) {
         when (intent?.action) {
             ACTION_FEED -> {
@@ -39,8 +41,7 @@ class FeedStarterService : IntentService("FeedStarterService") {
 
             ACTION_DISMISS -> {
                 val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                // TODO: Replace 0 with the notification ID
-                notificationManager.cancel(0)
+                notificationManager.cancel(STARTER_NOTIF_ID)
             }
         }
     }
@@ -52,11 +53,9 @@ class FeedStarterService : IntentService("FeedStarterService") {
     private fun handleActionFeed(starterUUID: String?, starterName: String?) {
         Log.d("Tag", "UUID: $starterUUID Name: $starterName")
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // TODO: Replace 0 with the notification ID
-        notificationManager.cancel(0)
+        notificationManager.cancel(STARTER_NOTIF_ID)
         // Notify the server
-        // TODO: Replace this with a constant or something
-        val baseURL = URL("http://192.168.1.68:8080")
+        val baseURL = URL(SERVICE_URL)
         val retrofit = Retrofit.Builder()
             .baseUrl(baseURL)
             .addConverterFactory(GsonConverterFactory.create())
